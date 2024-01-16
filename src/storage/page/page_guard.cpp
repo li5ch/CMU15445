1efd82bacdfb1b321beffe2375c18033636ca5dc
li5ch <darkfalmer@gmail.com>
2024-01-16 21:06:56 +0800

    begin pro1:完成bpm

--- src/storage/page/page_guard.cpp
+++ src/storage/page/page_guard.cpp
@@ -2,8 +2,25 @@
 #include "buffer/buffer_pool_manager.h"
 
 namespace bustub {
+/** TODO(P1): Add implementation
+ *
+ * @brief Move constructor for BasicPageGuard
+ *
+ * When you call BasicPageGuard(std::move(other_guard)), you
+ * expect that the new guard will behave exactly like the other
+ * one. In addition, the old page guard should not be usable. For
+ * example, it should not be possible to call .Drop() on both page
+ * guards and have the pin count decrease by 2.
+ */
+BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {
+  bpm_ = that.bpm_;
+  is_dirty_ = that.is_dirty_;
+  page_ = that.page_;
 
-BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {}
+  that.bpm_ = nullptr;
+  that.is_dirty_ = false;
+  that.page_ = nullptr;
+}
 
 void BasicPageGuard::Drop() {}
 

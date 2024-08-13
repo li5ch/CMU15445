388505cdc361a682f21a7560add6192775391515
li5ch <darkfalmer@gmail.com>
2024-08-14 00:01:21 +0800

    finish basic exe

--- src/execution/index_scan_executor.cpp
+++ src/execution/index_scan_executor.cpp
@@ -12,32 +12,32 @@
 #include "execution/executors/index_scan_executor.h"
 
 namespace bustub {
-    IndexScanExecutor::IndexScanExecutor(ExecutorContext *exec_ctx, const IndexScanPlanNode *plan)
-        : AbstractExecutor(exec_ctx), plan_(plan) {}
+	IndexScanExecutor::IndexScanExecutor(ExecutorContext *exec_ctx, const IndexScanPlanNode *plan)
+		: AbstractExecutor(exec_ctx), plan_(plan),
+		  tree_{dynamic_cast<BPlusTreeIndexForTwoIntegerColumn *>(exec_ctx_->GetCatalog()->GetIndex(
+			  plan_->GetIndexOid())->index_.get())}, iter_{tree_->GetBeginIterator()} {}
 
-    void IndexScanExecutor::Init() {
-        tree_ = dynamic_cast<BPlusTreeIndexForTwoIntegerColumn *>(exec_ctx_->GetCatalog()->GetIndex(
-            plan_->GetIndexOid())->index_.get());
-        iter_ = tree_->GetBeginIterator();
-        tableInfo = exec_ctx_->GetCatalog()->GetTable(exec_ctx_->GetCatalog()->GetIndex(
-            plan_->GetIndexOid())->table_name_);
-    }
+	void IndexScanExecutor::Init() {
+		tableInfo = exec_ctx_->GetCatalog()->GetTable(exec_ctx_->GetCatalog()->GetIndex(
+			plan_->GetIndexOid())->table_name_);
+	}
 
-    auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+	auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
 
-        while (!iter_.IsEnd()) {
-            const auto &pair = *iter_;
-            auto res = tableInfo->table_->GetTuple(pair.second);
-            *rid = pair.second;
-            if (res.first.is_deleted_) {
-                ++iter_;
-            } else {
-                *tuple = res.second;
-                return true;
-            }
+		while (!iter_.IsEnd()) {
+			const auto &pair = *iter_;
+			auto res = tableInfo->table_->GetTuple(pair.second);
+			*rid = pair.second;
+			if (res.first.is_deleted_) {
+				++iter_;
+			} else {
+				*tuple = res.second;
+				++iter_;
+				return true;
+			}
 
-        }
-        return false;
-    }
+		}
+		return false;
+	}
 
 }  // namespace bustub

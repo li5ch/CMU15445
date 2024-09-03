99e599ca7beb72aab6dabc27bdeba5507d891c76
li5ch <darkfalmer@gmail.com>
2024-09-03 21:02:50 +0800

    nlj join

--- src/include/execution/executors/nested_loop_join_executor.h
+++ src/include/execution/executors/nested_loop_join_executor.h
@@ -25,40 +25,40 @@ namespace bustub {
 /**
  * NestedLoopJoinExecutor executes a nested-loop JOIN on two tables.
  */
-	class NestedLoopJoinExecutor : public AbstractExecutor {
-	public:
-		/**
-		 * Construct a new NestedLoopJoinExecutor instance.
-		 * @param exec_ctx The executor context
-		 * @param plan The nested loop join plan to be executed
-		 * @param left_executor The child executor that produces tuple for the left side of join
-		 * @param right_executor The child executor that produces tuple for the right side of join
-		 */
-		NestedLoopJoinExecutor(ExecutorContext *exec_ctx, const NestedLoopJoinPlanNode *plan,
-							   std::unique_ptr<AbstractExecutor> &&left_executor,
-							   std::unique_ptr<AbstractExecutor> &&right_executor);
+    class NestedLoopJoinExecutor : public AbstractExecutor {
+    public:
+        /**
+         * Construct a new NestedLoopJoinExecutor instance.
+         * @param exec_ctx The executor context
+         * @param plan The nested loop join plan to be executed
+         * @param left_executor The child executor that produces tuple for the left side of join
+         * @param right_executor The child executor that produces tuple for the right side of join
+         */
+        NestedLoopJoinExecutor(ExecutorContext *exec_ctx, const NestedLoopJoinPlanNode *plan,
+                               std::unique_ptr<AbstractExecutor> &&left_executor,
+                               std::unique_ptr<AbstractExecutor> &&right_executor);
 
-		/** Initialize the join */
-		void Init() override;
+        /** Initialize the join */
+        void Init() override;
 
-		/**
-		 * Yield the next tuple from the join.
-		 * @param[out] tuple The next tuple produced by the join
-		 * @param[out] rid The next tuple RID produced, not used by nested loop join.
-		 * @return `true` if a tuple was produced, `false` if there are no more tuples.
-		 */
-		auto Next(Tuple *tuple, RID *rid) -> bool override;
+        /**
+         * Yield the next tuple from the join.
+         * @param[out] tuple The next tuple produced by the join
+         * @param[out] rid The next tuple RID produced, not used by nested loop join.
+         * @return `true` if a tuple was produced, `false` if there are no more tuples.
+         */
+        auto Next(Tuple *tuple, RID *rid) -> bool override;
 
-		/** @return The output schema for the insert */
-		auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); };
+        /** @return The output schema for the insert */
+        auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); };
 
-	private:
-		/** The NestedLoopJoin plan node to be executed. */
-		const NestedLoopJoinPlanNode *plan_;
-		std::unique_ptr<AbstractExecutor> left_executor_, right_executor_;
-		std::vector<Tuple> right_tuple_;
-		int right_idx{0};
-		Tuple left_tuple_;
-	};
+    private:
+        /** The NestedLoopJoin plan node to be executed. */
+        const NestedLoopJoinPlanNode *plan_;
+        std::unique_ptr<AbstractExecutor> left_executor_, right_executor_;
+        std::vector<Tuple> right_tuple_;
+        int right_idx{-1};
+        Tuple left_tuple_;
+    };
 
 }  // namespace bustub

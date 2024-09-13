2c8a5e1305dfadc69d1ba17b156c7836077eb3dc
li5ch <darkfalmer@gmail.com>
2024-09-13 19:18:20 +0800

    feat:username长度校验

--- src/concurrency/lock_manager.cpp
+++ src/concurrency/lock_manager.cpp
@@ -18,78 +18,78 @@
 
 namespace bustub {
 
-	auto LockManager::LockTable(Transaction *txn, LockMode lock_mode, const table_oid_t &oid) -> bool {
-		if (txn->IsTableExclusiveLocked(oid)) {
+    auto LockManager::LockTable(Transaction *txn, LockMode lock_mode, const table_oid_t &oid) -> bool {
+        if (txn->IsTableExclusiveLocked(oid)) {
 
-		}
-		return true;
-	}
+        }
+        return true;
+    }
 
-	auto LockManager::UnlockTable(Transaction *txn, const table_oid_t &oid) -> bool { return true; }
+    auto LockManager::UnlockTable(Transaction *txn, const table_oid_t &oid) -> bool { return true; }
 
-	auto LockManager::LockRow(Transaction *txn, LockMode lock_mode, const table_oid_t &oid, const RID &rid) -> bool {
-		return true;
-	}
+    auto LockManager::LockRow(Transaction *txn, LockMode lock_mode, const table_oid_t &oid, const RID &rid) -> bool {
+        return true;
+    }
 
-	auto LockManager::UnlockRow(Transaction *txn, const table_oid_t &oid, const RID &rid, bool force) -> bool {
-		return true;
-	}
+    auto LockManager::UnlockRow(Transaction *txn, const table_oid_t &oid, const RID &rid, bool force) -> bool {
+        return true;
+    }
 
-	void LockManager::UnlockAll() {
-		// You probably want to unlock all table and txn locks here.
-	}
+    void LockManager::UnlockAll() {
+        // You probably want to unlock all table and txn locks here.
+    }
 
-	void LockManager::AddEdge(txn_id_t t1, txn_id_t t2) {}
+    void LockManager::AddEdge(txn_id_t t1, txn_id_t t2) {}
 
-	void LockManager::RemoveEdge(txn_id_t t1, txn_id_t t2) {}
+    void LockManager::RemoveEdge(txn_id_t t1, txn_id_t t2) {}
 
-	auto LockManager::HasCycle(txn_id_t *txn_id) -> bool { return false; }
+    auto LockManager::HasCycle(txn_id_t *txn_id) -> bool { return false; }
 
-	auto LockManager::GetEdgeList() -> std::vector<std::pair<txn_id_t, txn_id_t>> {
-		std::vector<std::pair<txn_id_t, txn_id_t>> edges(0);
-		return edges;
-	}
+    auto LockManager::GetEdgeList() -> std::vector<std::pair<txn_id_t, txn_id_t>> {
+        std::vector<std::pair<txn_id_t, txn_id_t>> edges(0);
+        return edges;
+    }
 
-	void LockManager::RunCycleDetection() {
-		while (enable_cycle_detection_) {
-			std::this_thread::sleep_for(cycle_detection_interval);
-			{  // TODO(students): detect deadlock
-			}
-		}
-	}
+    void LockManager::RunCycleDetection() {
+        while (enable_cycle_detection_) {
+            std::this_thread::sleep_for(cycle_detection_interval);
+            {  // TODO(students): detect deadlock
+            }
+        }
+    }
 
-	auto LockManager::UpgradeLockTable(Transaction *txn, LockMode lock_mode, const table_oid_t &oid) -> bool {
-		return false;
-	}
+    auto LockManager::UpgradeLockTable(Transaction *txn, LockMode lock_mode, const table_oid_t &oid) -> bool {
+        return false;
+    }
 
-	auto
-	LockManager::UpgradeLockRow(Transaction *txn, LockMode lock_mode, const table_oid_t &oid, const RID &rid) -> bool {
-		return false;
-	}
+    auto
+    LockManager::UpgradeLockRow(Transaction *txn, LockMode lock_mode, const table_oid_t &oid, const RID &rid) -> bool {
+        return false;
+    }
 
-	auto LockManager::AreLocksCompatible(LockMode l1, LockMode l2) -> bool {
+    auto LockManager::AreLocksCompatible(LockMode l1, LockMode l2) -> bool {
+        return matrix_[int(l1)][int(l2)];
+    }
 
-		return false;
-	}
+    auto LockManager::CanTxnTakeLock(Transaction *txn, LockMode lock_mode) -> bool {
 
-	auto LockManager::CanTxnTakeLock(Transaction *txn, LockMode lock_mode) -> bool {
-		return false;
-	}
+        return false;
+    }
 
-	void LockManager::GrantNewLocksIfPossible(LockRequestQueue *lock_request_queue) {}
+    void LockManager::GrantNewLocksIfPossible(LockRequestQueue *lock_request_queue) {}
 
-	auto LockManager::CanLockUpgrade(LockMode curr_lock_mode, LockMode requested_lock_mode) -> bool {
-		return false;
-	}
+    auto LockManager::CanLockUpgrade(LockMode curr_lock_mode, LockMode requested_lock_mode) -> bool {
+        return false;
+    }
 
-	auto
-	LockManager::CheckAppropriateLockOnTable(Transaction *txn, const table_oid_t &oid, LockMode row_lock_mode) -> bool {
-		return false;
-	}
+    auto
+    LockManager::CheckAppropriateLockOnTable(Transaction *txn, const table_oid_t &oid, LockMode row_lock_mode) -> bool {
+        return false;
+    }
 
-	auto LockManager::FindCycle(txn_id_t source_txn, std::vector<txn_id_t> &path, std::unordered_set<txn_id_t> &on_path,
-								std::unordered_set<txn_id_t> &visited, txn_id_t *abort_txn_id) -> bool {
-		return false;
-	}
+    auto LockManager::FindCycle(txn_id_t source_txn, std::vector<txn_id_t> &path, std::unordered_set<txn_id_t> &on_path,
+                                std::unordered_set<txn_id_t> &visited, txn_id_t *abort_txn_id) -> bool {
+        return false;
+    }
 
 }  // namespace bustub

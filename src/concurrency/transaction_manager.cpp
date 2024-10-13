baf4dd93e795922e2bd37ac68d15c314da90c600
li5ch <darkfalmer@gmail.com>
2024-10-13 23:31:43 +0800

    begin p4.3

--- src/concurrency/transaction_manager.cpp
+++ src/concurrency/transaction_manager.cpp
@@ -20,25 +20,40 @@
 #include "catalog/catalog.h"
 #include "common/macros.h"
 #include "storage/table/table_heap.h"
+
 namespace bustub {
 
-void TransactionManager::Commit(Transaction *txn) {
-  // Release all the locks.
-  ReleaseLocks(txn);
+	void TransactionManager::Commit(Transaction *txn) {
+		// Release all the locks.
+		ReleaseLocks(txn);
 
-  txn->SetState(TransactionState::COMMITTED);
-}
+		txn->SetState(TransactionState::COMMITTED);
+	}
 
-void TransactionManager::Abort(Transaction *txn) {
-  /* TODO: revert all the changes in write set */
+	void TransactionManager::Abort(Transaction *txn) {
+		/* revert all the changes in write set */
+		for (auto &record: *txn->GetWriteSet()) {
+			if (record.table_heap_->GetTuple(record.rid_).first.is_deleted_) {
+				record.table_heap_->UpdateTupleMeta(TupleMeta{INVALID_TXN_ID, txn->GetTransactionId(), false},
+													record.rid_);
+			} else {
+				record.table_heap_->UpdateTupleMeta(TupleMeta{INVALID_TXN_ID, INVALID_TXN_ID, true}, record.rid_);
+			}
+		}
+		std::for_each(txn->GetIndexWriteSet()->begin(), txn->GetIndexWriteSet()->end(),
+					  [&](IndexWriteRecord i) {
+						  if (i.wtype_ == WType::DELETE)
+							  i.catalog_->GetIndex(i.index_oid_)->index_->InsertEntry(i.tuple_, i.rid_, txn);
+						  else {
+							  i.catalog_->GetIndex(i.index_oid_)->index_->DeleteEntry(i.tuple_, i.rid_, txn);
+						  }
+					  });
+		ReleaseLocks(txn);
+		txn->SetState(TransactionState::ABORTED);
+	}
 
-  ReleaseLocks(txn);
+	void TransactionManager::BlockAllTransactions() { UNIMPLEMENTED("block is not supported now!"); }
 
-  txn->SetState(TransactionState::ABORTED);
-}
-
-void TransactionManager::BlockAllTransactions() { UNIMPLEMENTED("block is not supported now!"); }
-
-void TransactionManager::ResumeTransactions() { UNIMPLEMENTED("resume is not supported now!"); }
+	void TransactionManager::ResumeTransactions() { UNIMPLEMENTED("resume is not supported now!"); }
 
 }  // namespace bustub
--- src/execution/delete_executor.cpp
+++ src/execution/delete_executor.cpp
@@ -16,39 +16,59 @@
 
 namespace bustub {
 
-    DeleteExecutor::DeleteExecutor(ExecutorContext *exec_ctx, const DeletePlanNode *plan,
-                                   std::unique_ptr<AbstractExecutor> &&child_executor)
-        : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
+	DeleteExecutor::DeleteExecutor(ExecutorContext *exec_ctx, const DeletePlanNode *plan,
+								   std::unique_ptr<AbstractExecutor> &&child_executor)
+		: AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-    void DeleteExecutor::Init() {
-        child_executor_->Init();
-    }
+	void DeleteExecutor::Init() {
+		child_executor_->Init();
+		try {
+			GetExecutorContext()->GetLockManager()->LockTable(GetExecutorContext()->GetTransaction(),
+															  LockManager::LockMode::INTENTION_EXCLUSIVE,
+															  plan_->GetTableOid());
+		} catch (TransactionAbortException e) {
+			throw ExecutionException("insert get table lock failed");
+		}
 
-    auto DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
-        if (is_end_) return false;
-        Tuple delete_tuple;
-        RID empty_rid;
-        int delete_count = 0;
-        auto table_info = GetExecutorContext()->GetCatalog()->GetTable(plan_->GetTableOid());
-        while (1) {
-            if (child_executor_->Next(&delete_tuple, &empty_rid)) {
-                table_info->table_->UpdateTupleMeta(TupleMeta{INVALID_TXN_ID, INVALID_TXN_ID, true}, empty_rid);
-                auto index = GetExecutorContext()->GetCatalog()->GetTableIndexes(table_info->name_);
-                for (auto &i: index) {
-                    auto k = delete_tuple.KeyFromTuple(table_info->schema_, i->key_schema_,
-                                                       i->index_->GetKeyAttrs());
-                    i->index_->DeleteEntry(k, empty_rid, exec_ctx_->GetTransaction());
-                }
-                delete_count++;
-            } else {
-                std::vector<Value> values{};
-                values.reserve(GetOutputSchema().GetColumnCount());
-                values.emplace_back(TypeId::INTEGER, delete_count);
-                *tuple = Tuple{values, &GetOutputSchema()};
-                is_end_ = true;
-                return true;
-            }
-        }
-    }
+	}
+
+	auto DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+		if (is_end_) return false;
+		Tuple delete_tuple;
+		RID empty_rid;
+		int delete_count = 0;
+		auto delete_txn_id = INVALID_TXN_ID;
+		auto txn = GetExecutorContext()->GetTransaction();
+		if (GetExecutorContext()->IsDelete())delete_txn_id = txn->GetTransactionId();
+		auto table_info = GetExecutorContext()->GetCatalog()->GetTable(plan_->GetTableOid());
+		while (1) {
+			if (child_executor_->Next(&delete_tuple, &empty_rid)) {
+				table_info->table_->UpdateTupleMeta(TupleMeta{INVALID_TXN_ID, delete_txn_id, true}, empty_rid);
+				auto index = GetExecutorContext()->GetCatalog()->GetTableIndexes(table_info->name_);
+				for (auto &i: index) {
+					auto k = delete_tuple.KeyFromTuple(table_info->schema_, i->key_schema_,
+													   i->index_->GetKeyAttrs());
+					i->index_->DeleteEntry(k, empty_rid, exec_ctx_->GetTransaction());
+
+				}
+				std::for_each(index.begin(), index.end(),
+							  [&table_info, &exec_ctx = exec_ctx_, &empty_rid, &delete_tuple](IndexInfo *i) {
+								  exec_ctx->GetTransaction()->AppendIndexWriteRecord(
+									  IndexWriteRecord(empty_rid, table_info->oid_, WType::DELETE, delete_tuple,
+													   i->index_oid_, exec_ctx->GetCatalog()));
+							  });
+				auto rw = TableWriteRecord(table_info->oid_, empty_rid, table_info->table_);
+				txn->AppendTableWriteRecord(rw);
+				delete_count++;
+			} else {
+				std::vector<Value> values{};
+				values.reserve(GetOutputSchema().GetColumnCount());
+				values.emplace_back(TypeId::INTEGER, delete_count);
+				*tuple = Tuple{values, &GetOutputSchema()};
+				is_end_ = true;
+				return true;
+			}
+		}
+	}
 
 }  // namespace bustub
